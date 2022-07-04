b3b5d47f422002e22cd2ca0ceccf9075c5421d8a
9mande <gudori888@naver.com>
2022-07-05 02:47:58 +0900

    add tab 4 (look fragment)

--- app/src/main/java/com/example/myapplication/WeatherData.java
+++ app/src/main/java/com/example/myapplication/WeatherData.java
@@ -3,8 +3,6 @@ package com.example.myapplication;/* Java 1.8 샘플 코드 */
 
 import static java.lang.Integer.parseInt;
 
-import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,49 +16,76 @@ import java.io.IOException;
 
 public class WeatherData {
     private static final String serviceKey = "smqvpocvWHGwD1w9ADvTBw9I0a0n2sh2uknYbSmip4giy5JJaQtWurR7rJZEQk9VGgDJKFoG41BWy1xiQ8yAHg%3D%3D";
-    private String nx = "60";	//위도
-    private String ny = "125";	//경도
-    private String baseDate = "20210531";	//조회하고싶은 날짜
-    private String baseTime = "0500";	//조회하고싶은 시간
+    private String nx;	//위도
+    private String ny;	//경도
+    private String baseDate;	//조회하고싶은 날짜
+    private String baseTime;	//조회하고싶은 시간
     private String type = "json";	//조회하고 싶은 type(json, xml 중 고름)
 
     private int weather, temperature;
 
-    public WeatherData(String x, String y, String baseDate){
+    public WeatherData(String x, String y, String baseDate, String baseTime){
         this.nx = x;
         this.ny = y;
         this.baseDate = baseDate;
+        this.baseTime = timeChange(baseTime);
     }
 
     public int getWeather() throws IOException, JSONException {
+        boolean fail = true;
+        StringBuilder sb;
+        do {
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
+            urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /*‘21년 6월 28일발표*/
+            urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /*05시 발표*/
+            urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); /*예보지점의 X 좌표값*/
+            urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); /*예보지점의 Y 좌표값*/
 
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode("20210628", "UTF-8")); /*‘21년 6월 28일발표*/
-        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode("0500", "UTF-8")); /*05시 발표*/
-        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(20000);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+            conn.connect();
+
+            BufferedReader rd;
+
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            String line;
+            sb = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            String result = sb.toString();
+
+            // response 키를 가지고 데이터를 파싱
+            JSONObject jsonObj_1 = new JSONObject(result);
+            String response = jsonObj_1.getString("response");
+
+            JSONObject jsonObj_test = new JSONObject(response);
+            String header = jsonObj_test.getString("header");
+
+            JSONObject jsonObj_test2 = new JSONObject(header);
+            String code = jsonObj_test2.getString("resultCode");
+
+            baseDate = String.valueOf(parseInt(baseDate) - 1);
+            fail = code == "03";
+
+        }while(fail);
 
         String result = sb.toString();
 
@@ -75,7 +100,6 @@ public class WeatherData {
         // body 로 부터 items 찾기
         JSONObject jsonObj_3 = new JSONObject(body);
         String items = jsonObj_3.getString("items");
-        Log.i("ITEMS", items);
 
         // items로 부터 itemlist 를 받기
         JSONObject jsonObj_4 = new JSONObject(items);
@@ -85,8 +109,6 @@ public class WeatherData {
             jsonObj_4 = jsonArray.getJSONObject(i);
             String fcstValue = jsonObj_4.getString("fcstValue");
             String category = jsonObj_4.getString("category");
-
-            Log.d("asdf", fcstValue);
 
             if (category.equals("SKY")) {
                 if (fcstValue.equals("1")) {
@@ -100,11 +122,58 @@ public class WeatherData {
                 }
             }
 
-            if (category.equals("T3H") || category.equals("T1H")) {
+            if (category.equals("TMP")) {
                 temperature = parseInt(fcstValue);
             }
         }
 
         return weather*100+temperature;
+    }
+
+
+    public String timeChange(String time)
+    {
+        switch(time) {
+
+            case "0200":
+            case "0300":
+            case "0400":
+                time = "0200";
+                break;
+            case "0500":
+            case "0600":
+            case "0700":
+                time = "0500";
+                break;
+            case "0800":
+            case "0900":
+            case "1000":
+                time = "0800";
+                break;
+            case "1100":
+            case "1200":
+            case "1300":
+                time = "1100";
+                break;
+            case "1400":
+            case "1500":
+            case "1600":
+                time = "1400";
+                break;
+            case "1700":
+            case "1800":
+            case "1900":
+                time = "1700";
+                break;
+            case "2000":
+            case "2100":
+            case "2200":
+                time = "2000";
+                break;
+            default:
+                time = "2300";
+
+        }
+        return time;
     }
 }
--- app/src/main/java/com/example/myapplication/fragment_look.java
+++ app/src/main/java/com/example/myapplication/fragment_look.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static java.lang.Integer.parseInt;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +33,20 @@ public class fragment_look extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View rootView;
+
+    private SimpleDateFormat sdf, sdf2;
+    private Date date;
+
+    ImageView look_weather;
+    TextView look_temperature;
+    TextView look_when;
+
+    private static final String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    private static final String[] weathers = {"맑음", "비", "구름", "흐림"};
+    private String weather;
+    private int temperature;
 
     public fragment_look() {
         // Required empty public constructor
@@ -53,12 +77,79 @@ public class fragment_look extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        sdf = new SimpleDateFormat("yyyyMMdd HHmm");
+        date = new Date(System.currentTimeMillis());
+        String date_string = (String) sdf.format(date);
+        String dates = date_string.split(" ")[0];
+        String times = date_string.split(" ")[1];
+        times = times.substring(0, 2) + "00";
+
+        WeatherData weatherData = new WeatherData("67", "101", dates, times);
+        Thread weatherThread = new Thread(){
+            public void run(){
+                int tmp = 0;
+                try {
+                    tmp = weatherData.getWeather();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                weather = weathers[tmp/100];
+                temperature = tmp%100;
+            }
+        };
+        weatherThread.start();
+        try{
+            weatherThread.join();
+            update(rootView);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_look, container, false);
+        rootView = inflater.inflate(R.layout.fragment_look, container, false);
+        look_weather = rootView.findViewById(R.id.look_weather);
+        look_temperature = rootView.findViewById(R.id.look_temperature);
+        look_when = rootView.findViewById(R.id.look_when);
+
+        if (!(look_weather == null && look_temperature == null && look_when == null)){
+            update(rootView);
+        }
+
+        return rootView;
     }
+
+    public void update(View rootView){
+        try{
+
+            if (weather == "맑음") {
+                look_weather.setImageResource(R.drawable.sun);
+
+            }else if (weather == "비") {
+                look_weather.setImageResource(R.drawable.rain);
+
+            }else if (weather == "구름") {
+                look_weather.setImageResource(R.drawable.cloud);
+
+            }else if (weather == "흐림") {
+                look_weather.setImageResource(R.drawable.sun_cloud);
+            }
+
+            look_temperature.setText(temperature + "℃");
+            sdf2 = new SimpleDateFormat("MM월 dd일 날씨");
+            look_when.setText((String)sdf2.format(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }
